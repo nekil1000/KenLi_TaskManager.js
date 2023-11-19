9052e90d5a121e2027331a867d476e08630133bf
nekil1000 <125524033+nekil1000@users.noreply.github.com>
2023-11-19 18:26:15 +0800

    Update Yourname_ModuleName.js

--- Yourname_ModuleName.js
+++ Yourname_ModuleName.js
@@ -1,10 +1,31 @@
+
+
 module.exports = {
-    // Explain what function A does
-    functionA() {
-        return 1+2;
+     tasks : [],
+    addTask(id , description , progress) {
+        this.tasks.push({id : id, description : description , progress : progress});
     },
-    // Explain what function B does
-    functionB() {
-        console.log("Hello function B");
-    }
+
+    getAllTasks() {
+        return this.tasks.map(task => ({ id: task.id, description: task.description, progress: task.progress }));
+    },
+
+    completeTask(taskId) {
+        this.tasks = this.tasks.filter(task => task.id !== taskId);
+    },
+
+    updateTask(taskId, updatedTask) {
+        this.tasks = this.tasks.map(task => (task.id === taskId ? updatedTask : task));
+    },
+
+    getTaskById(taskId) {
+        return this.tasks.find(task => task.id === taskId);
+    },
+
+    getTasksByStatus(status) {
+        return this.tasks.filter(task => task.progress === status);
+    },
+
+   
 }
+
